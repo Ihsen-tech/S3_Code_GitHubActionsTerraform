5a6d1eb3888f5150e90da82dbc8159af1c33bcc3
Ihsen-tech <ihsen.lagha@capgemini.com>
2024-11-14 14:30:28 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -69,6 +69,7 @@ resource "null_resource" "link_monitoring" {
       # Login to Azure CLI (Linux operating system assumed)
       az login --service-principal -u $con_client_id -p $con_client_secret --tenant $con_tenant_id
       # TODO your scripting code
+      az webapp config appsettings set --name $web_app_name --resource-group $rg_name --settings APPINSIGHTS_INSTRUMENTATIONKEY=$inst_key APPINSIGHTS_PROFILERFEATURE_VERSION=1.0.0 APPINSIGHTS_SNAPSHOTFEATURE_VERSION=1.0.0 APPLICATIONINSIGHTS_CONNECTION_STRING=$conn_str ApplicationInsightsAgent_EXTENSION_VERSION=~3 DiagnosticServices_EXTENSION_VERSION=~3 InstrumentationEngine_EXTENSION_VERSION=disabled SnapshotDebugger_EXTENSION_VERSION=disabled XDT_MicrosoftApplicationInsights_BaseExtensions=recommended XDT_MicrosoftApplicationInsights_PreemptSdk=disabled
     EOT
     environment = {
       // Parameters needed to login
